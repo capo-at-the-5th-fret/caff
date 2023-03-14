b0aec9c87a8f51a886617ee004e28084e5016e42
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2023-03-14 12:42:34 -0400

    Initial commit

---
+++ include/easy/bit.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+#include <concepts>
+
+namespace easy
+{
+    // clang-format off
+    template <typename T>
+    concept enum_bitmask = std::is_enum_v<T> &&
+        std::is_unsigned_v<std::underlying_type_t<T>> &&
+    requires(T t)
+    {
+        //T::none;
+        //T::all;
+        { ~t } -> std::same_as<T>;
+        { t & t } -> std::same_as<T>;
+        { t | t } -> std::same_as<T>;
+        { t ^ t } -> std::same_as<T>;
+        { t &= t } -> std::same_as<T&>;
+        { t |= t } -> std::same_as<T&>;
+        { t ^= t } -> std::same_as<T&>;
+    };
+    // clang-format on
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr void set(B& b, B bit, Bs... bits) noexcept
+    {
+        b |= (bit | ... | bits);
+    }
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr void reset(B& b, Bs... bits) noexcept
+    {
+        if constexpr (sizeof...(Bs) > 0)
+        {
+            b &= ~(... | bits);
+        }
+        else
+        {
+            b = static_cast<B>(0);
+        }
+    }
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr bool test(B b, B bit, Bs... bits) noexcept
+    {
+        const auto test_bits = (bit | ... | bits);
+        return ((b & test_bits) == test_bits);
+    }
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr void flip(B& b, B bit, Bs... bits) noexcept
+    {
+        b ^= (bit | ... | bits);
+    }
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr bool test_all_of(B b, B bit, Bs... bits) noexcept
+    {
+        const auto test_bits = (bit | ... | bits);
+        return ((b & test_bits) == test_bits);
+    }
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr bool test_any_of(B b, B bit, Bs... bits) noexcept
+    {
+        return ((b & (bit | ... | bits)) != static_cast<B>(0));
+    }
+
+    template <enum_bitmask B, enum_bitmask... Bs>
+    constexpr bool test_none_of(B b, B bit, Bs... bits) noexcept
+    {
+        return ((b & (bit | ... | bits)) == static_cast<B>(0));
+    }
+}
+
+namespace ez = easy;
---
+++ tests/bit_tests.cpp
@@ -0,0 +1,196 @@
+#include <doctest/doctest.h>
+#include "easy/bit.hpp"
+
+namespace
+{
+    enum class options : std::uint8_t
+    {
+        none = 0,
+        read = 0x01,
+        write = 0x02,
+        append = 0x04,
+        trunc = 0x08
+    };
+
+    constexpr options operator~(options rhs) noexcept
+    {
+        using ut = std::underlying_type_t<options>;
+        return static_cast<options>(~static_cast<ut>(rhs));
+    }
+    
+    constexpr options& operator&=(options& lhs, options rhs) noexcept
+    {
+        using ut = std::underlying_type_t<options>;
+        lhs = static_cast<options>(static_cast<ut>(lhs) & static_cast<ut>(rhs));
+        return lhs;
+    }
+    
+    constexpr options& operator|=(options& lhs, options rhs) noexcept
+    {
+        using ut = std::underlying_type_t<options>;
+        lhs = static_cast<options>(static_cast<ut>(lhs) | static_cast<ut>(rhs));
+        return lhs;
+    }
+    
+    constexpr options& operator^=(options& lhs, options rhs) noexcept
+    {
+        using ut = std::underlying_type_t<options>;
+        lhs = static_cast<options>(static_cast<ut>(lhs) ^ static_cast<ut>(rhs));
+        return lhs;
+    }
+    
+    constexpr options operator&(options lhs, options rhs) noexcept
+    {
+        lhs &= rhs;
+        return lhs;
+    }
+    
+    constexpr options operator|(options lhs, options rhs) noexcept
+    {
+        lhs |= rhs;
+        return lhs;
+    }
+    
+    constexpr options operator^(options lhs, options rhs) noexcept
+    {
+        lhs ^= rhs;
+        return lhs;
+    }
+}
+
+TEST_CASE("enum bitmask")
+{
+    using namespace ez;
+    
+    options o = options::none;
+
+    SUBCASE("set single bit at a time")
+    {
+        set(o, options::write);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE_FALSE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+
+        set(o, options::append);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+    }
+
+    SUBCASE("set multiple bits at a time")
+    {
+        set(o, options::write, options::append);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+    }
+
+    SUBCASE("reset single bit at a time")
+    {
+        set(o, options::write, options::append);
+        reset(o, options::write);
+        REQUIRE_FALSE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+
+        reset(o, options::append);
+        REQUIRE(o == options::none);
+    }
+
+    SUBCASE("reset multiple bits at a time")
+    {
+        set(o, options::write, options::append);
+        reset(o, options::write, options::append);
+        REQUIRE(o == options::none);
+    }
+
+    SUBCASE("reset all bits")
+    {
+        set(o, options::write, options::append);
+        reset(o);
+        REQUIRE(o == options::none);
+    }
+
+    SUBCASE("test single bit at a time")
+    {
+        set(o, options::write, options::append);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+    }
+
+    SUBCASE("test multiple bits at a time")
+    {
+        set(o, options::write, options::append);
+        REQUIRE(test(o, options::write, options::append));
+        REQUIRE_FALSE(test(o, options::write, options::read));
+        REQUIRE_FALSE(test(o, options::write, options::trunc));
+        REQUIRE_FALSE(test(o, options::read, options::write));
+        REQUIRE_FALSE(test(o, options::trunc, options::write));
+        REQUIRE_FALSE(test(o, options::append, options::read));
+        REQUIRE_FALSE(test(o, options::append, options::trunc));
+        REQUIRE_FALSE(test(o, options::read, options::append));
+        REQUIRE_FALSE(test(o, options::trunc, options::append));
+        REQUIRE_FALSE(test(o, options::read, options::trunc));
+    }
+
+    SUBCASE("flip single bit at a time")
+    {
+        flip(o, options::write);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE_FALSE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+
+        flip(o, options::append);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+
+        flip(o, options::write);
+        REQUIRE_FALSE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+
+        flip(o, options::append);
+        REQUIRE(o == options::none);
+    }
+
+    SUBCASE("flip multiple bits at a time")
+    {
+        flip(o, options::write, options::append);
+        REQUIRE(test(o, options::write));
+        REQUIRE_FALSE(test(o, options::read));
+        REQUIRE(test(o, options::append));
+        REQUIRE_FALSE(test(o, options::trunc));
+
+        flip(o, options::write, options::append);
+        REQUIRE(o == options::none);
+    }
+
+    SUBCASE("test_xxx")
+    {
+        set(o, options::write, options::append);
+
+        REQUIRE(test_all_of(o, options::write));
+        REQUIRE(test_all_of(o, options::append));
+        REQUIRE(test_all_of(o, options::write, options::append));
+        REQUIRE_FALSE(
+            test_all_of(o, options::write, options::append, options::read));
+
+        REQUIRE(test_any_of(o, options::write));
+        REQUIRE(test_any_of(o, options::append));
+        REQUIRE_FALSE(test_any_of(o, options::read));
+
+        REQUIRE(test_none_of(o, options::read));
+        REQUIRE_FALSE(test_none_of(o, options::write));
+        REQUIRE_FALSE(test_none_of(o, options::append));
+    }
+}
