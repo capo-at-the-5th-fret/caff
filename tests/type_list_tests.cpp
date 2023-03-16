2819534d06f7e022aa9e8bf3108aeb74f0d21414
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2023-03-16 12:48:08 -0400

    Cosmetic change to type_list_tests.cpp

--- tests/type_list_tests.cpp
+++ tests/type_list_tests.cpp
@@ -6,16 +6,16 @@ TEST_CASE("standard_character_types")
     using T = easy::standard_character_types;
 
     static_assert(std::tuple_size_v<T> == 5);
-    static_assert(std::is_same_v<std::tuple_element_t<0, T>, char>);
-    static_assert(std::is_same_v<std::tuple_element_t<1, T>, wchar_t>);
-    static_assert(std::is_same_v<std::tuple_element_t<2, T>, char8_t>);
-    static_assert(std::is_same_v<std::tuple_element_t<3, T>, char16_t>);
-    static_assert(std::is_same_v<std::tuple_element_t<4, T>, char32_t>);
+    static_assert(std::is_same_v<std::tuple_element_t<0,T>, char>);
+    static_assert(std::is_same_v<std::tuple_element_t<1,T>, wchar_t>);
+    static_assert(std::is_same_v<std::tuple_element_t<2,T>, char8_t>);
+    static_assert(std::is_same_v<std::tuple_element_t<3,T>, char16_t>);
+    static_assert(std::is_same_v<std::tuple_element_t<4,T>, char32_t>);
 
     REQUIRE(std::tuple_size_v<T> == 5);
-    REQUIRE(std::is_same_v<std::tuple_element_t<0, T>, char>);
-    REQUIRE(std::is_same_v<std::tuple_element_t<1, T>, wchar_t>);
-    REQUIRE(std::is_same_v<std::tuple_element_t<2, T>, char8_t>);
-    REQUIRE(std::is_same_v<std::tuple_element_t<3, T>, char16_t>);
-    REQUIRE(std::is_same_v<std::tuple_element_t<4, T>, char32_t>);
+    REQUIRE(std::is_same_v<std::tuple_element_t<0,T>, char>);
+    REQUIRE(std::is_same_v<std::tuple_element_t<1,T>, wchar_t>);
+    REQUIRE(std::is_same_v<std::tuple_element_t<2,T>, char8_t>);
+    REQUIRE(std::is_same_v<std::tuple_element_t<3,T>, char16_t>);
+    REQUIRE(std::is_same_v<std::tuple_element_t<4,T>, char32_t>);
 }
