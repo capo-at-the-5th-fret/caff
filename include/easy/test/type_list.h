932d71d538cff540c7f293bc1df545d636084f0f
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2023-03-19 07:06:17 -0400

    Added primary_types for testing purposes

---
+++ include/easy/test/type_list.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "easy/type_list.h"
+#include "easy/test/dummy.h"
+
+namespace easy::test
+{
+    // A sample type from each of the 14 primary type categories
+    // REFERENCE:
+    // https://en.cppreference.com/w/cpp/header/type_traits
+
+    using primary_types = easy::tuple_cat_t
+    <
+        std::tuple<void, std::nullptr_t>,
+        integral_types,
+        standard_floating_point_types,
+        std::tuple
+        <
+            int[],
+            test::dummy_enum,
+            test::dummy_union,
+            test::dummy_class,
+            decltype(test::dummy_function),
+            int*,
+            int&,
+            int&&,
+            decltype(&test::dummy_class::member_variable),
+            decltype(&test::dummy_class::member_function)
+        >
+    >;
+}
