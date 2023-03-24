f8ca814cc3326b0a3af1f2b54312d59a6508836b
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2023-03-24 09:05:22 -0400

    Added source_location

---
+++ tests/source_location_tests.cpp
@@ -0,0 +1,21 @@
+#include <doctest/doctest.h>
+#include "easy/source_location.h"
+
+#include <string_view>
+
+TEST_CASE("source_location")
+{
+    constexpr auto sl = easy::source_location::current();
+
+#if __has_include(<source_location>) && __cpp_lib_source_location
+    static_assert(sl.line() == 8);
+    //static_assert(sl.column() == 56);
+    //static_assert(sl.file_name() == std::string_view{ "file name" });
+    //static_assert(sl.function_name() == std::string_view{ "function name" });
+#else
+    static_assert(sl.line() == 1);
+    static_assert(sl.column() == 1);
+    static_assert(sl.file_name() == std::string_view{ "file name" });
+    static_assert(sl.function_name() == std::string_view{ "function name" });
+#endif
+}
