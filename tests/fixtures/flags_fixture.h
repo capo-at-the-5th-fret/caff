f55b29a73bc9615fd98bafec5c2df5d569ef93b2
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2024-11-07 08:29:26 -0500

    Added flags class

---
+++ tests/fixtures/flags_fixture.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include "easy/test/compare.h"
+#include "easy/flags.h"
+
+namespace easy::test
+{
+    namespace dummy
+    {
+        enum class option : std::uint8_t
+        {
+            none = 0,
+            read = 0x01,
+            write = 0x02,
+            append = 0x04,
+            trunc = 0x08
+        };
+
+        using options = easy::flags<option>;
+
+        constexpr options options_all = options
+        {
+            option::read, option::write, option::append, option::trunc
+        };
+
+        struct options_fixture
+        {
+            static constexpr std::initializer_list<option> base_option_list = { option::write, option::append };
+            static constexpr std::initializer_list<option> diff_option_list = { option::read, option::trunc };
+
+            options base{ base_option_list };
+            options diff{ diff_option_list };
+
+            std::vector<std::tuple<options, equality_comparator>> comparisons =
+            {
+                { base, equality_equal },
+                //{ options{  }, equality_not_equal },
+                //{ options{ option::read }, equality_not_equal },
+                //{ options{ option::write, option::append, option::trunc }, equality_not_equal },
+                //{ options_all, equality_not_equal }
+            };
+        };
+    }
+}
