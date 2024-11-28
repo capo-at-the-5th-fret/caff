735d1471216724750244a812b200f9787d8a0867
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2024-11-28 09:55:35 -0500

    Same fix as last, but for tuple_enumerate

--- tests/tuple_tests.cpp
+++ tests/tuple_tests.cpp
@@ -484,7 +484,9 @@ TEMPLATE_TEST_CASE("tuple_enumerate", "[tuple]", tuple_test_type, pair_test_type
             ++element;
         });
 
-        REQUIRE(t == fixture.modified_expected );
+        const bool ok = (t == fixture.modified_expected);
+        REQUIRE(ok);
+        //REQUIRE(t == fixture.modified_expected );
     }
 
     SECTION("verify const correctness")
